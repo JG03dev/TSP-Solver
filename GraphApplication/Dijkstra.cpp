70fba604b37cadfe3e377aba0dbdf27708e6d0f2
YssfDevOps <youssef.cah.7912@gmail.com>
2023-10-04 19:08:12 +0200

    Dijkstra + Dijkstra queue + greedy not done yet

--- GraphApplication/Dijkstra.cpp
+++ GraphApplication/Dijkstra.cpp
@@ -1,12 +1,41 @@
 #include "pch.h"
 #include "Graph.h"
-
+#include <queue>
+#include <stack>
+#include <vector>
 // =============================================================================
 // Dijkstra ====================================================================
 // =============================================================================
 
 void Dijkstra(CGraph& graph, CVertex *pStart)
 {
+
+	for (CVertex& v : graph.m_Vertices)
+	{
+		v.m_DijkstraDistance = numeric_limits<double>::max();
+		v.m_DijkstraVisit = false;
+	}
+
+	pStart->m_DijkstraDistance = 0;
+	CVertex* pActual = pStart;
+
+	while (pActual != NULL)
+	{
+		for (CEdge* e : pActual->m_Edges)
+			if (e->m_pDestination->m_DijkstraDistance > pActual->m_DijkstraDistance + e->m_Length)
+				e->m_pDestination->m_DijkstraDistance = pActual->m_DijkstraDistance + e->m_Length;
+		
+		pActual->m_DijkstraVisit = true;
+		
+		pActual = NULL;
+		double min = numeric_limits<double>::max();
+		for (CVertex& v : graph.m_Vertices)
+			if (!v.m_DijkstraVisit && v.m_DijkstraDistance < min)
+			{
+				min = v.m_DijkstraDistance;
+				pActual = &v;
+			}		
+	}
 }
 
 // =============================================================================
@@ -15,4 +44,38 @@ void Dijkstra(CGraph& graph, CVertex *pStart)
 
 void DijkstraQueue(CGraph& graph, CVertex *pStart)
 {
+	struct comparator {
+		bool operator()(CVertex* pV1, CVertex* pV2) {
+			return pV1->m_DijkstraDistance > pV2->m_DijkstraDistance;
+		}
+	};
+	priority_queue<CVertex*, std::vector<CVertex*>, comparator> queue;
+
+	for (CVertex & v : graph.m_Vertices)
+	{
+		v.m_DijkstraDistance = numeric_limits<double>::max();
+		v.m_DijkstraVisit = false;
+	}
+	pStart->m_DijkstraDistance = 0;
+	queue.push(pStart);
+
+	CVertex* va = NULL;
+
+	while (!queue.empty())
+	{
+		va = queue.top();
+		queue.pop();
+		if (!va->m_DijkstraVisit)
+		{
+			for (CEdge* e : va->m_Edges)
+			{
+				if (e->m_pDestination->m_DijkstraDistance > va->m_DijkstraDistance + e->m_Length)
+				{
+					e->m_pDestination->m_DijkstraDistance = va->m_DijkstraDistance + e->m_Length;
+					queue.push(e->m_pDestination);
+				}
+			}
+			va->m_DijkstraVisit = true;
+		}
+	}	
 }
--- GraphApplication/Greedy.cpp
+++ GraphApplication/Greedy.cpp
@@ -1,8 +1,56 @@
 #include "pch.h"
 #include "Graph.h"
+#include <queue>
+#include <list>
 // SalesmanTrackGreedy =========================================================
 
 CTrack SalesmanTrackGreedy(CGraph& graph, CVisits &visits)
 {
-	return CTrack(&graph);
+	if (graph.m_Vertices.empty() || graph.m_Edges.empty())
+		return CTrack(&graph);
+	
+	struct comparator {
+		bool operator()(CVertex* pV1, CVertex* pV2) {
+			return pV1->m_DijkstraDistance > pV2->m_DijkstraDistance;
+		}
+	};
+	priority_queue<CVertex*, std::vector<CVertex*>, comparator> candidats;
+
+	for (CVertex* v : visits.m_Vertices)
+		if (v != visits.m_Vertices.front() && v != visits.m_Vertices.back())
+			candidats.push(v);
+	
+
+	CVertex* vActual = visits.m_Vertices.front();
+	CVertex* vNext = NULL;
+
+	list<CEdge*> camino;
+
+	while (!candidats.empty())
+	{
+		Dijkstra(graph, vActual);
+		vNext = candidats.top();
+
+		/*DEBO BUSCAR ENTRE LOS MAS PEQUEÑOS A PARTIR DE VACTUAL, BORRAR BUCLE DE ABAJO Y QUITAR PRIORITY QUEUE (creo)*/
+
+		for (CEdge* edge : vActual->m_Edges)
+		{
+			if (edge->m_pDestination == vNext)
+			{
+				camino.push_back(edge);
+				break;
+			}
+		}
+
+		candidats.pop();
+		vActual = vNext;
+	}
+
+	CTrack track(&graph);
+	for (CEdge* edge : camino)
+	{
+		track.AddLast(edge);
+	}
+
+	return track;
 }
