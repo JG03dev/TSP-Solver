55a369a53d8b5bdf6053f24bb0f86ae63a12277b
YssfDevOps <youssef.cah.7912@gmail.com>
2023-10-27 10:24:02 +0200

    backtracking updated

--- GraphApplication/Backtracking.cpp
+++ GraphApplication/Backtracking.cpp
@@ -13,28 +13,43 @@ stack<CEdge*> pilaCamins;
 double LenCamiMesCurt;
 double LenCamiActual;
 
-void SalesmanTrackBacktrackingRec(CVertex* pActual)
+bool visitedAllNodes(CGraph& g, CVisits& visits) {
+	for (CVertex* v : visits.m_Vertices) {
+		std::string nom = v->m_Name;
+		CVertex* s = g.GetVertex(nom.c_str());
+		if (!s->m_JaHePassat)
+			return false;
+	}
+}
+
+void SalesmanTrackBacktrackingRec(CVertex* pActual, CGraph& g, CVisits &visits)
 {
 	if (pActual == vDesti) {
-		if (LenCamiActual < LenCamiMesCurt) {
-			CamiCurt.Clear();
-			while (!pilaCamins.empty()) {
-				CamiCurt.m_Edges.push_front(pilaCamins.top());
-				pilaCamins.pop();
+		if (visitedAllNodes(g, visits))
+		{
+			if (LenCamiActual < LenCamiMesCurt) {
+				CamiCurt.Clear();
+				while (!pilaCamins.empty()) {
+					CamiCurt.m_Edges.push_back(pilaCamins.top());
+					pilaCamins.pop();
+				}
+				LenCamiMesCurt = LenCamiActual;
 			}
-			LenCamiMesCurt = LenCamiActual;
-		}
+		}	
 	}
 	else if (LenCamiActual < LenCamiMesCurt) {
 		// Pas endevant:
 		pActual->m_JaHePassat = true;
-		for (CEdge* pE : pActual->m_Edges) {
-			if (!pE->m_pDestination->m_JaHePassat) {
-				LenCamiActual += pE->m_Length;
-				pilaCamins.push(pE);
-				SalesmanTrackBacktrackingRec(pE->m_pDestination);
-				LenCamiActual -= pE->m_Length;
-			}
+		for (CEdge* pE : pActual->m_Edges) 
+		{
+			if ((pE == pilaCamins.top() && pE->m_pDestination->m_JaHePassat)) //No revisitar un node dins d'un cami d'un node a un altre
+				continue;
+
+			// ACUERDATE DE SI SE CUMPLE LA CONDICION, PONER EN g de CGRAPH en true EL JA HE PASSAT PARA QUE FUNCIONE BIEN EL VISITEDALLNODES.
+			LenCamiActual += pE->m_Length;
+			pilaCamins.push(pE);
+			SalesmanTrackBacktrackingRec(pE->m_pDestination, g, visits);
+			LenCamiActual -= pE->m_Length;
 		}
 		// Pas enrere:
 		pActual->m_JaHePassat = false;
@@ -47,7 +62,7 @@ void SalesmanTrackBacktrackingRec(CVertex* pActual)
 
 CTrack SalesmanTrackBacktracking(CGraph &graph, CVisits &visits)
 {
-	if (graph.m_Vertices.empty())
+	if (graph.m_Vertices.size() < 2 || visits.m_Vertices.size() < 2)
 		return CTrack(&graph);
 	
 	CVertex* pInici = visits.m_Vertices.front();
@@ -55,15 +70,12 @@ CTrack SalesmanTrackBacktracking(CGraph &graph, CVisits &visits)
 	CamiCurt.Clear();
 	LenCamiMesCurt = numeric_limits<double>::max();
 	LenCamiActual = 0.0;
+
 	for (CVertex& v : graph.m_Vertices) v.m_JaHePassat = false;
 
-	CTrack resCami(&graph);
+	SalesmanTrackBacktrackingRec(pInici, visits);
 
-	for (CVertex* v : visits.m_Vertices) {
-		SalesmanTrackBacktrackingRec(v);
-		resCami.Append(CamiCurt);
-	}
-	return resCami;
+	return CamiCurt;
 }
 
 
