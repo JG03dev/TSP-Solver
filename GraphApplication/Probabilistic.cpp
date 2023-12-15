6835be4cf6103d371f0320da32d4ddda7b1a1e4d
YssfDevOps <youssef.cah.7912@gmail.com>
2023-12-15 10:22:11 +0100

    Update Probabilistic.cpp

--- GraphApplication/Probabilistic.cpp
+++ GraphApplication/Probabilistic.cpp
@@ -24,14 +24,10 @@ CTrack getDijkstraPrevio(CVertex* vo, CVertex* vd)
 
 CTrack SalesmanTrackProbabilistic(CGraph& graph, CVisits& visits)
 {
+	return CTrack(&graph);
+
 	if (graph.m_Vertices.size() < 2 || visits.m_Vertices.size() < 2)
 		return CTrack(&graph);
-
-	// Hay que pasar por todos los que sean vertex to visit.
-	for (CVertex& v : graph.m_Vertices) {
-		v.m_JaHePassat = false;
-	}
-
 	CVertex* pInici = visits.m_Vertices.front();
 	CVertex* vDesti = visits.m_Vertices.back();
 
@@ -42,7 +38,7 @@ CTrack SalesmanTrackProbabilistic(CGraph& graph, CVisits& visits)
 		std::vector<std::pair<CTrack, double>> vector_dijkstra;
 		for (CVertex* vd : visits.m_Vertices) // Vertex Desti
 		{
-			if (vo == vd || vo == pInici) {
+			if (vo == vd) {
 				vector_dijkstra.emplace_back(std::make_pair(CTrack(NULL), 0.0));
 			}
 			else {
@@ -53,65 +49,63 @@ CTrack SalesmanTrackProbabilistic(CGraph& graph, CVisits& visits)
 		matrix_dijkstra.push_back(vector_dijkstra);
 	}
 
+
+
 	// CHANGE FROM HERE DOWN BELOW!
-	int i = 0;
-	/*
-	priority_queue<CBBNodeAlumne*, std::vector<CBBNodeAlumne*>, comparator> queue;
 
-	// Inicialization of the first node.
-	queue.push(new CBBNodeAlumne(visits.GetNVertices(), 0));
+	std::vector<int> indexVisites(visits.GetNVertices());
 
-	double maxLength = numeric_limits<double>::max();
-	std::vector<int> optimum;
-	while (!queue.empty()) {
-		CBBNodeAlumne* pS = queue.top();
-		queue.pop();
-
-		int pV = pS->m_VisitesFetes[pS->m_numFets - 1]; // Ultim node del cami.
-		if (pS->m_VisitesFetes.back() != -1) { // Desti arribat!
-			// Solucion encontrada
-			optimum = pS->m_VisitesFetes;
-			delete pS;
-			break;
-		}
-		else if (pS->m_VisitesFetes[visits.GetNVertices() - 2] != -1) // Cuando visitados todos.
-		{
-			double realDist = matrix_dijkstra[pV][pS->m_sizeVisits - 1].second;
-			double w = pS->m_Length + realDist; //Distancia g (no hay heuristica)
-
-			if (w < maxLength) {
-				CBBNodeAlumne* pSNew = new CBBNodeAlumne(*pS, pS->m_sizeVisits - 1, w);
-
-				maxLength = w; //Solucio parcial, actualitzem maxLength per podar
-				queue.push(pSNew);
-			}
-		}
-		else {
-			for (int i = 0; i < pS->m_sizeVisits - 1; i++) {
-				if (pS->m_VisitesAFer[i])  continue; //Recorrem nomes els que no hem visitat
-
-				double realDist = matrix_dijkstra[pV][i].second;
-				double w = pS->m_Length + realDist; //Distancia g (no hay heuristica)
-
-				if (w < maxLength) {
-					CBBNodeAlumne* pSNew = new CBBNodeAlumne(*pS, i, w);
-					if (pSNew->m_VisitesFetes.back() != -1) maxLength = w; //Solucio parcial, actualitzem maxLength per podar
-					queue.push(pSNew);
-				}
-			}
-		}
-		delete pS;
+	for (int i = 0; i < visits.GetNVertices(); ++i) {
+		indexVisites[i] = i;
 	}
 
-	CTrack Res(&graph);
-	for (int i = 1; i < optimum.size(); i++)
-	{
-		Res.Append(matrix_dijkstra[optimum[i - 1]][optimum[i]].first);
+	std::vector<bool> m_VisitesAFer;
+
+	double LongCamiActual = 0.0;
+	double LongCamiMesCurt = numeric_limits<double>::max();
+
+	// Inicialitzacio aleatoria
+	int origen = 0, desti = indexVisites.back();
+	// Shuffle the elements in between
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::vector<int> middleElements(indexVisites.begin() + 1, indexVisites.end() - 1);
+	std::shuffle(middleElements.begin(), middleElements.end(), gen);
+
+	// Reconstruct the shuffled array
+	indexVisites.clear();
+	indexVisites.push_back(origen);
+	indexVisites.insert(indexVisites.end(), middleElements.begin(), middleElements.end());
+	indexVisites.push_back(desti);
+
+
+	for (int i = 1; i < visits.GetNVertices() - 2; ++i) {
+		for (int j = i + 1; j < visits.GetNVertices() - 1; ++j) {
+			if (i == j) continue;
+			int iVisites = indexVisites[i], jVisites = indexVisites[j];
+
+			double cami_aux = LongCamiActual;
+
+			if (iVisites != jVisites - 1 && iVisites != jVisites + 1) {
+				LongCamiActual = LongCamiActual - matrix_dijkstra[iVisites - 1][iVisites].second - matrix_dijkstra[iVisites][iVisites + 1].second -
+					matrix_dijkstra[jVisites - 1][jVisites].second - matrix_dijkstra[jVisites][jVisites + 1].second + matrix_dijkstra[iVisites - 1][jVisites].second + matrix_dijkstra[j][iVisites + 1].second
+					+ matrix_dijkstra[jVisites - 1][iVisites].second + matrix_dijkstra[iVisites][jVisites + 1].second;
+			}
+			else {
+				// mirar formula foto
+				LongCamiActual = LongCamiActual - matrix_dijkstra[iVisites - 1][iVisites].second - matrix_dijkstra[jVisites][jVisites + 1].second +
+					matrix_dijkstra[iVisites - 1][jVisites].second + matrix_dijkstra[iVisites][jVisites + 1].second;
+			}
+
+			if (LongCamiActual < LongCamiMesCurt) {
+				LongCamiMesCurt = LongCamiActual;
+			}
+			else {
+				// Si no hem aconseguit un camí més curt, tornem a l'estat anterior
+				LongCamiActual = cami_aux;
+			}
+		}
 	}
-
-	return Res;
-	*/
-
 
 	return CTrack(&graph);
 }
